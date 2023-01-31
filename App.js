e10a5a0214232a7aff3f070c0e867478dea86508
DHEERAJ2AUS <108796274+DHEERAJ2AUS@users.noreply.github.com>
2023-01-31 19:06:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,47 +1,82 @@
 var newTask = document.querySelector("#create-task");
+var openTasks = document.querySelector(".append-task");
 var modal = document.querySelector(".modal");
 var closeSpan = document.querySelector(".close");
+var allCardContents = document.querySelectorAll("#card-contents");
 var btn = document.querySelector("#add-task");
+
+//count of tasks
 let count = 1;
 
+//previous id of task
+let prevId;
 
+
+//Create a new task
 newTask.addEventListener('click', () => {
     modal.style.display = "block";
+    // console.log(openTasks.childElementCount);
+    if(allCardContents[0].childElementCount === 0 &&
+        allCardContents[1].childElementCount === 0 &&
+        allCardContents[2].childElementCount === 0 &&
+        allCardContents[3].childElementCount === 0){
+        
+        //If there is no task, the serial no. will start from 1
+        count = 1;
+    }
 });
 
+//close modal
 closeSpan.addEventListener('click', () => {
     modal.style.display = "none";
 });
 
+//close modal on clicking wherever in the window
 window.onclick = function(event){
     if(event.target == modal){
         modal.style.display = "none";
     }
 }
 
+//allowing drop to different cards
 function allowDrop(e){
     e.preventDefault();
 }
 
+//allowing drag of tasks
 function drag(e){
     e.dataTransfer.setData("task-card", e.target.id);
+    prevId = e.target.parentNode.parentNode.id;
 }
 
+
+//drop will work only if the previous source element id of the task is same as the target element class
+//Users can only proceed in the given sequence "Open" -> "In-Progress" -> "In-Review" -> "Done"
 function drop(e){
-    e.preventDefault();
-    var data = e.dataTransfer.getData("task-card");
-    if(e.target.id === "card-contents"){
-        e.target.appendChild(document.getElementById(data));
+    if(
+        prevId == e.target.classList[0]
+    ){
+        e.preventDefault();
+        var data = e.dataTransfer.getData("task-card");
+        if(e.target.id === "card-contents"){
+            e.target.appendChild(document.getElementById(data));
+        }
+    }else{
+        // console.log('PrevId: ' +prevId);
+        // console.log('NextClass: '+e.target.parentNode.id);
+        alert('You cannot go from "'+prevId+'" to "'+e.target.parentNode.id+'".');
+
     }
 }
 
+//Deletion of a created/completed task
 function deleteTask(event){
     let delNode = event.currentTarget.parentNode.parentNode;
     delNode.removeChild(event.currentTarget.parentNode);
 }
 
+//Creation of a task
 btn.addEventListener('click', () => {
-    var openTasks = document.querySelector(".append-task");
     let taskName = document.querySelector("#task-name");
     let taskDesc = document.querySelector("#task-description");
     if(taskName.value === ""){
@@ -81,6 +116,4 @@ btn.addEventListener('click', () => {
     modal.style.display = "none";
     count++;
 });
-
-
 
